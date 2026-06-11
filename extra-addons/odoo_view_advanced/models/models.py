222c7c5c92d7ebf1c835e4553eb6f46ec37246cb
merzamac <100945727+merzamac@users.noreply.github.com>
2026-06-11 11:36:31 -0400

    add:wizard

--- extra-addons/odoo_view_advanced/models/models.py
+++ extra-addons/odoo_view_advanced/models/models.py
@@ -1,6 +1,7 @@
 # -*- coding: utf-8 -*-
 from odoo import models, fields, api, exceptions
-
+import io
+import base64
 
 class CustomItem(models.Model):
     _name = 'odoo_view_advanced.custom_item'
@@ -9,8 +10,38 @@ class CustomItem(models.Model):
     unit_price = fields.Char(string='Precio unitario')
 
 
+class UploadFile(models.TransientModel):
+    _name = "odoo_view_advanced.upload_file"
+    
+    upload_file = fields.Binary(string="Subir Fichero",required=True)
+    file_name = fields.Char(string="Nombre del Fichero")
 
+    def import_file(self):
+        if self.file_name:
+            if '.csv' not in self.file_name:
+                raise exceptions.ValidationError("El archivo debe ser 'csv'")
+            file = self.read_file_from_binary(self.upload_file)
+            lines = file.splitlines()
+            for line in lines:
+                elements = line.split(',')
+                if len(elements)>1:
+                    self.env['odoo_view_advanced.custom_item'].create(
+                        {
+                            'name':elements[0],
+                            'unit_price':float(elements[1]),
+                        }
+                    )
 
+    def read_file_from_binary(self,file):
+        try:
+            with io.BytesIO(base64.b64decode(file)) as f:
+                f.seek(0)
+                return f.read().decode('UTF-8')
+        except Exception as e:
+            print(str(e))
+            raise e
+            
+        
 
 
 
