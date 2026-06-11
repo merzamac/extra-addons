35f109fd1f572d69c2513da739090ae88c41971b
merzamac <100945727+merzamac@users.noreply.github.com>
2026-06-11 09:47:58 -0400

    material base

---
+++ extra-addons/odoo_view_advanced/models/models.py
@@ -0,0 +1,19 @@
+# -*- coding: utf-8 -*-
+from odoo import models, fields, api, exceptions
+
+
+class CustomItem(models.Model):
+    _name = 'odoo_view_advanced.custom_item'
+
+    name = fields.Char(string='Descripción')
+    unit_price = fields.Char(string='Precio unitario')
+
+
+
+
+
+
+
+
+
+
